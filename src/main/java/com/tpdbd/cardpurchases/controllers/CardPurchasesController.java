914064b1b112915acfad7c9d6b45900f557750a8
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-03-23 00:44:59 -0300

    Add/use new test services

--- src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
+++ src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
@@ -1,5 +1,9 @@
 package com.tpdbd.cardpurchases.controllers;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,9 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tpdbd.cardpurchases.model.Bank;
 import com.tpdbd.cardpurchases.model.Discount;
 import com.tpdbd.cardpurchases.services.CardPurchasesService;
 
+/**
+ * All endpoint paths that start with "tests" are endpoints that
+ * were no asked to be created, but are necessary for unit test
+ */
 @RestController
 class CardPurchasesController {
 
@@ -21,8 +30,21 @@ class CardPurchasesController {
         return "Card Purchases application";
     }
 
-    @PostMapping("/bank/{bankCuit}/addDiscountPromotion")
-    void addDiscountPromotion(@PathVariable String bankCuit, @RequestBody Discount discount) {
-        service.addDiscountPromotion(bankCuit, discount);
+    @GetMapping("/tests/banks/cuits")
+    Map<String, List<String>> testGetBanks() {
+        var result = new LinkedHashMap<String, List<String>>();
+        result.put("cuits", this.service.testsBanksGetCuits());
+        return result;
     }
+
+    @GetMapping("/tests/banks/{cuit}")
+    Bank testGetBank(@PathVariable String cuit) {
+        return this.service.testsBanksGetBank(cuit);
+    }
+
+    @PostMapping("/banks/{cuit}/addDiscountPromotion")
+    void banksAddDiscountPromotion(@PathVariable String cuit, @RequestBody Discount discount) {
+        this.service.banksAddDiscountPromotion(cuit, discount);
+    }
+
 }
--- src/main/java/com/tpdbd/cardpurchases/services/CardPurchasesServiceImpl.java
+++ src/main/java/com/tpdbd/cardpurchases/services/CardPurchasesServiceImpl.java
@@ -1,8 +1,13 @@
 package com.tpdbd.cardpurchases.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tpdbd.cardpurchases.errors.BankNotFoundException;
+import com.tpdbd.cardpurchases.model.Bank;
 import com.tpdbd.cardpurchases.model.Discount;
 import com.tpdbd.cardpurchases.repositories.BankRepository;
 
@@ -15,9 +20,34 @@ public class CardPurchasesServiceImpl implements CardPurchasesService {
 
     @Override
     @Transactional
-    public void addDiscountPromotion(String bankCuit, Discount discount) {
-        bankRepository.findByCuit(bankCuit).ifPresent(bank -> {
-            bank.addPromotion(discount);
+    public List<String> testsBanksGetCuits() {
+        var cuits = new ArrayList<String>();
+
+        this.bankRepository.findAll().forEach(bank -> {
+            cuits.add(bank.getCuit());
         });
+
+        return cuits;
+    }
+
+    @Override
+    @Transactional
+    public Bank testsBanksGetBank(String cuit) {
+        System.out.println("testsBanksGetBank: Bank = " + cuit);
+        return this.bankRepository
+                .findByCuit(cuit)
+                .orElseThrow(() -> new BankNotFoundException(cuit));
+    }
+
+    @Override
+    @Transactional
+    public void banksAddDiscountPromotion(String cuit, Discount discount) {
+        System.out.println("banksAddDiscountPromotion: Bank = " + cuit);
+        var bank = this.bankRepository
+                .findByCuit(cuit)
+                .orElseThrow(() -> new BankNotFoundException(cuit))
+                .addPromotion(discount);
+
+        this.bankRepository.save(bank);
     }
 }
