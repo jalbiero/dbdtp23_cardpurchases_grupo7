1a475b6542a8ef4b6bb2a09d9e4902e2ba4249fc
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-03-23 23:46:29 -0300

    Move test endpoint to its own controller

--- src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
+++ src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
@@ -1,27 +1,18 @@
 package com.tpdbd.cardpurchases.controllers;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tpdbd.cardpurchases.model.Bank;
 import com.tpdbd.cardpurchases.model.Discount;
 import com.tpdbd.cardpurchases.services.CardPurchasesService;
 
-/**
- * All endpoint paths that start with "tests" are endpoints that
- * were no asked to be created, but are necessary for unit test
- */
 @RestController
-class CardPurchasesController {
-
+public class CardPurchasesController {
     @Autowired
     private CardPurchasesService service;
 
@@ -30,21 +21,13 @@ class CardPurchasesController {
         return "Card Purchases application";
     }
 
-    @GetMapping("/tests/banks/cuits")
-    Map<String, List<String>> testGetBanks() {
-        var result = new LinkedHashMap<String, List<String>>();
-        result.put("cuits", this.service.testsBanksGetCuits());
-        return result;
-    }
-
-    @GetMapping("/tests/banks/{cuit}")
-    Bank testGetBank(@PathVariable String cuit) {
-        return this.service.testsBanksGetBank(cuit);
-    }
-
     @PostMapping("/banks/{cuit}/addDiscountPromotion")
     void banksAddDiscountPromotion(@PathVariable String cuit, @RequestBody Discount discount) {
         this.service.banksAddDiscountPromotion(cuit, discount);
     }
 
+    @PutMapping("/payments/{code}/updateDates")
+    void paymentsUpdateDates(@PathVariable String code /*, @RequestBody Discount discount*/) {
+        //this.service.
+    }
 }
