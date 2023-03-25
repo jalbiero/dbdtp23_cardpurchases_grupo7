fec6946f2658dfce1474b786ce9149a6a9c82f27
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-03-24 21:36:48 -0300

    Initial commit

---
+++ src/main/java/com/tpdbd/cardpurchases/services/TestService.java
@@ -0,0 +1,19 @@
+package com.tpdbd.cardpurchases.services;
+
+import java.util.List;
+
+import com.tpdbd.cardpurchases.model.Bank;
+import com.tpdbd.cardpurchases.model.Payment;
+
+/**
+ * Provide all extra funcionallity needed for manual or automated testing
+ */
+public interface TestService {
+    List<String> getBankCuits();
+
+    Bank getBank(String cuit);
+
+    List<String> getPaymentCodes();
+
+    Payment getPayment(String code);
+}
