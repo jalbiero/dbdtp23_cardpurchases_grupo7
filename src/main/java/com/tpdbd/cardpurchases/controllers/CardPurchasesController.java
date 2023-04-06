cacf752c3e19eec2f1708130cba3b3d966df8cff
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-06 00:20:53 -0300

    Add DTO types

--- src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
+++ src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
@@ -1,6 +1,8 @@
 package com.tpdbd.cardpurchases.controllers;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tpdbd.cardpurchases.controllers.util.Params;
-import com.tpdbd.cardpurchases.model.Card;
+import com.tpdbd.cardpurchases.dto.RequestDTO;
+import com.tpdbd.cardpurchases.dto.ResponseDTO;
 import com.tpdbd.cardpurchases.services.CardPurchasesService;
-
-import jakarta.annotation.Nullable;
 
 @RestController
 public class CardPurchasesController {
@@ -27,20 +27,22 @@ public class CardPurchasesController {
     }
 
     @PostMapping("/banks/{cuit}/addDiscountPromotion")
-    void banksAddDiscountPromotion(@PathVariable String cuit, @RequestBody Params.Discount discount) {
+    void banksAddDiscountPromotion(@PathVariable String cuit, @RequestBody RequestDTO.Discount discount) {
         this.service.banksAddDiscountPromotion(cuit, discount);
     }
 
     @PutMapping("/payments/{code}/updateDates")
-    void paymentsUpdateDates(@PathVariable String code, @RequestBody Params.PaymentDates paymentDates) {
+    void paymentsUpdateDates(@PathVariable String code, @RequestBody RequestDTO.PaymentDates paymentDates) {
         this.service.paymentsUpdateDates(code, paymentDates.firstExpiration(), paymentDates.secondExpiration());
     }
 
     @GetMapping("/cards/getNextExpire")
-    List<Card> cardsGetNextExpired(@Nullable @RequestBody(required = false) Params.NextExpiredCards nextExpiredCards) {
-        if (nextExpiredCards != null)
-            return this.service.cardsGetNextExpire(nextExpiredCards.baseDate(), nextExpiredCards.daysFromBaseDate());
-        else
-            return this.service.cardsGetNextExpire(null, null);
+    Set<ResponseDTO.Card> cardsGetNextExpired(@RequestBody RequestDTO.NextExpiredCards nextExpiredCards) {
+        var cards = this.service.cardsGetNextExpire(
+                nextExpiredCards.baseDate(), nextExpiredCards.daysFromBaseDate());
+
+        return cards.stream()
+                .map(ResponseDTO.Card::fromModel)
+                .collect(Collectors.toSet());
     }
 }
