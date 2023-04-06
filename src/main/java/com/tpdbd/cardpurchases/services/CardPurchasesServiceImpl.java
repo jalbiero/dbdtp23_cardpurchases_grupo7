24a3a3046caed7bb0a69e4bd209f843a419e45e4
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-06 00:21:51 -0300

    Minor refactoring

--- src/main/java/com/tpdbd/cardpurchases/services/CardPurchasesServiceImpl.java
+++ src/main/java/com/tpdbd/cardpurchases/services/CardPurchasesServiceImpl.java
@@ -1,18 +1,17 @@
 package com.tpdbd.cardpurchases.services;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tpdbd.cardpurchases.controllers.util.Params;
+import com.tpdbd.cardpurchases.dto.RequestDTO;
 import com.tpdbd.cardpurchases.errors.BankNotFoundException;
 import com.tpdbd.cardpurchases.errors.PaymentNotFoundException;
 import com.tpdbd.cardpurchases.model.Card;
-import com.tpdbd.cardpurchases.model.Discount;
 import com.tpdbd.cardpurchases.repositories.BankRepository;
 import com.tpdbd.cardpurchases.repositories.CardRepository;
 import com.tpdbd.cardpurchases.repositories.PaymentRepository;
@@ -33,24 +32,12 @@ public class CardPurchasesServiceImpl implements CardPurchasesService {
 
     @Override
     @Transactional
-    public void banksAddDiscountPromotion(String cuit, Params.Discount discount) {
+    public void banksAddDiscountPromotion(String cuit, RequestDTO.Discount discount) {
         var bank = this.bankRepository
                 .findByCuit(cuit)
                 .orElseThrow(() -> new BankNotFoundException(cuit));
 
-        bank.addPromotion(new Discount(
-                bank,
-                discount.code(),
-                discount.promotionTitle(),
-                discount.nameStore(),
-                discount.cuitStore(),
-                discount.validityStartDate(),
-                discount.validityEndDate(),
-                discount.comments(),
-                discount.discountPercentage(),
-                discount.priceCap(),
-                discount.onlyCash()));
-
+        bank.addPromotion(RequestDTO.Discount.toModel(discount, bank));
         this.bankRepository.save(bank);
     }
 
@@ -68,10 +55,9 @@ public class CardPurchasesServiceImpl implements CardPurchasesService {
     }
 
     @Override
-    public List<Card> cardsGetNextExpire(@Nullable LocalDate baseDate, @Nullable Integer daysFromBaseDate) {
-        // TODO Add proper filtering
+    public Set<Card> cardsGetNextExpire(@Nullable LocalDate baseDate, @Nullable Integer daysFromBaseDate) {
+        // // TODO Add proper filtering, just for now, all cards
         return StreamSupport.stream(this.cardRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
-
+                .collect(Collectors.toSet());
     }
 }
