b65e396489e9a3352b4b83042ca3c07d70b8397e
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-06 00:21:14 -0300

    Add operations for Cards

--- src/main/java/com/tpdbd/cardpurchases/repositories/CardRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/CardRepository.java
@@ -1,9 +1,16 @@
 package com.tpdbd.cardpurchases.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tpdbd.cardpurchases.model.Card;
 
-// @todo Check its usage, maybe CrudRepository<Card, Long> is simpler
 public interface CardRepository extends CrudRepository<Card, Long> {
+    Optional<Card> findByNumber(String number);
+
+    @Query("SELECT u.number FROM Card u")
+    List<String> findAllNumbers();
 }
