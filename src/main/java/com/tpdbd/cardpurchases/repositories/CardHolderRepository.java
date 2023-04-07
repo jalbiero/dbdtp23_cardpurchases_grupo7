e0000b326c25e2a0997c5d1d6be8c549ef14b1c6
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-07 00:44:30 -0300

    Add find by DNI

--- src/main/java/com/tpdbd/cardpurchases/repositories/CardHolderRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/CardHolderRepository.java
@@ -1,9 +1,11 @@
 package com.tpdbd.cardpurchases.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tpdbd.cardpurchases.model.CardHolder;
 
-// @todo Check its usage, maybe CrudRepository<CardHolder, Long> is simpler
 public interface CardHolderRepository extends CrudRepository<CardHolder, Long> {
+    Optional<CardHolder> findByDni(String dni);
 }
