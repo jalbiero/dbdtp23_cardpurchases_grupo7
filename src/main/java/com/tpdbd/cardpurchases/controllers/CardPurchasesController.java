72b165016cec8e4734628772b50cebc3b166b0f4
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-09 19:26:50 -0300

    Add documentation to each endpoint
    User List intead of Set for responses

--- src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
+++ src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
@@ -1,8 +1,7 @@
 package com.tpdbd.cardpurchases.controllers;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tpdbd.cardpurchases.dto.RequestDTO;
 import com.tpdbd.cardpurchases.dto.ResponseDTO;
+import com.tpdbd.cardpurchases.errors.BadRequestException;
 import com.tpdbd.cardpurchases.services.CardPurchasesService;
 
 @RestController
@@ -21,37 +21,123 @@ public class CardPurchasesController {
     @Autowired
     private CardPurchasesService service;
 
+    /***
+     * Endpoint to check the application is up and running 
+     * 
+     * URL:
+     *      GET /
+     * 
+     * Return:
+     *      The name of the application
+     */
     @GetMapping("/")
     String index() {
         return "Card Purchases application";
     }
 
+    /**
+     * Adds a new Discount promotion to the speficied bank (identified by its CUIT)
+     * 
+     * URL: 
+     *      POST /banks/{cuit}/addDiscountPromotion
+     * 
+     * ContentType: 
+     *      application/json
+     * 
+     * Params:
+     *  - URL: {cuit} string that indentifies the bank
+     *  - Body:
+     *      {
+     *          "code":               "Promotion code",
+     *          "promotionTitle":     "Promotion title", 
+     *          "nameStore":          "Store name", 
+     *          "cuitStore":          "Store CUIT",
+     *          "validityStartDate":  "2020/10/10",
+     *          "validityEndDate":    "2023/12/31", 
+     *          "comments":           "Some extra comments", 
+     *          "discountPercentage": 10.5, 
+     *          "priceCap":           10000.0, 
+     *          "onlyCash":           true
+     *      }
+     */
     @PostMapping("/banks/{cuit}/addDiscountPromotion")
     void banksAddDiscountPromotion(@PathVariable String cuit, @RequestBody RequestDTO.Discount discount) {
         this.service.banksAddDiscountPromotion(cuit, discount);
     }
 
+    /**
+     * Update the dates of the specified payment indentified by its code
+     * 
+     * URL: 
+     *      PUT /payments/{code}/updateDates
+     * 
+     * ContentType: 
+     *      application/json
+     * 
+     * Params:
+     *  - URL: {code} string that indentifies the payment
+     *  - Body:
+     *      {
+     *          "firstExpiration": "2023/10/31", 
+     *          "secondExpiration": "2023/11/15"
+     *      }
+     */    
     @PutMapping("/payments/{code}/updateDates")
     void paymentsUpdateDates(@PathVariable String code, @RequestBody RequestDTO.PaymentDates paymentDates) {
+        if (paymentDates.firstExpiration().isAfter(paymentDates.secondExpiration())) {
+            throw new BadRequestException(
+                "Second expiration date (%s) must be grater than the fist one (%s)", 
+                paymentDates.secondExpiration(), 
+                paymentDates.firstExpiration());
+        }
+        
         this.service.paymentsUpdateDates(code, paymentDates.firstExpiration(), paymentDates.secondExpiration());
     }
 
     /**
-     * List cards that expire in the following 'NextExpiredCards.daysFromBaseDate' days
-     * starting from 'NextExpiredCards.baseDate'.
+     * Lists cards that expire in the following days (by default 30 days starting
+     * from the moment of calling this endpoint if the days and the date are not 
+     * specified)
      *
-     * @param nextExpiredCards
-     * @return A list of cards
+     * URL: 
+     *      GET /cards/getSoonToExpire
+     * 
+     * ContentType: 
+     *      application/json
+     * 
+     * Params:
+     *  - URL: 
+     *  - Body (optional):
+     *      {
+     *          "baseDate": "2023/10/15",       
+     *          "daysFromBaseDate": 25 
+     *      }     
+     * 
+     * Return:
+     *      A list of Cards:
+     * 
+     *      [
+     *          {
+     *              "number": "44756745756",
+     *              "ccv", "123"
+     *              "cardholderNameInCard": "Juan Perez",
+     *              "since": "2023/01/01",
+     *              "expirationDate": "2026/01/01",
+     *              "bankCuit": "23-1231231231-9", 
+     *              "userDni": "30123978"          
+     *          },
+     *          ...
+     *      ]
      */
-    @GetMapping("/cards/getNextExpire")
-    Set<ResponseDTO.Card> cardsGetNextExpired(@RequestBody(required=false) 
-                                              Optional<RequestDTO.NextExpiredCards> nextExpiredCards) 
+    @GetMapping("/cards/getSoonToExpire")
+    List<ResponseDTO.Card> cardsGetSonToExpired(@RequestBody(required=false) 
+                                                Optional<RequestDTO.SoonToExpire> soonToExpire) 
     {
-        var params = nextExpiredCards.orElse(new RequestDTO.NextExpiredCards());
-        var cards = this.service.cardsGetNextExpire(params.baseDate(), params.daysFromBaseDate());
+        var params = soonToExpire.orElse(new RequestDTO.SoonToExpire());
+        var cards = this.service.cardsGetSoonToExpire(params.baseDate(), params.daysFromBaseDate());
 
         return cards.stream()
-                .map(ResponseDTO.Card::fromModel)
-                .collect(Collectors.toSet());
+            .map(ResponseDTO.Card::fromModel)
+            .toList();
     }
 }
