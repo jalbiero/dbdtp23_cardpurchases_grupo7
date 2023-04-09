f2101f0568b5984e5601ca9a21e1b11b240355da
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-09 00:20:55 -0300

    Add findAllDnis

--- src/main/java/com/tpdbd/cardpurchases/repositories/CardHolderRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/CardHolderRepository.java
@@ -1,11 +1,16 @@
 package com.tpdbd.cardpurchases.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tpdbd.cardpurchases.model.CardHolder;
 
 public interface CardHolderRepository extends CrudRepository<CardHolder, Long> {
     Optional<CardHolder> findByDni(String dni);
+
+    @Query("SELECT u.dni FROM CardHolder u")
+    List<String> findAllDnis();
 }
