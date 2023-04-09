0e7f803d9b25c6a2c78125c322a90e9f67b33128
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-09 19:27:36 -0300

    Better organization of errors

--- src/main/java/com/tpdbd/cardpurchases/errors/CardNotFoundException.java
+++ src/main/java/com/tpdbd/cardpurchases/errors/CardNotFoundException.java
@@ -1,11 +1,7 @@
 package com.tpdbd.cardpurchases.errors;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class CardNotFoundException extends RuntimeException {
+public class CardNotFoundException extends NotFoundException {
     public CardNotFoundException(String number) {
-        super(String.format("Card not found, number = %s", number));
+        super("Card not found, number = %s", number);
     }
 }
--- src/main/java/com/tpdbd/cardpurchases/errors/PaymentNotFoundException.java
+++ src/main/java/com/tpdbd/cardpurchases/errors/PaymentNotFoundException.java
@@ -1,11 +1,7 @@
 package com.tpdbd.cardpurchases.errors;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class PaymentNotFoundException extends RuntimeException {
+public class PaymentNotFoundException extends NotFoundException {
     public PaymentNotFoundException(String code) {
-        super(String.format("Payment not found, code = %s", code));
+        super("Payment not found, code = %s", code);
     }
 }
