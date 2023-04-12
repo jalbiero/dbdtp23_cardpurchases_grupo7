46a8473e3755f490e145078b6b665c3b7f49cbe2
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-04-11 23:53:14 -0300

    Add finder to locate purchases

--- src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
@@ -1,8 +1,11 @@
 package com.tpdbd.cardpurchases.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tpdbd.cardpurchases.model.Purchase;
 
 public interface PurchaseRepository<T extends Purchase> extends CrudRepository<T, Long> {
+    List<T> findByCuitStoreAndCardNumber(String cuitStore, String cardNumber);
 }
