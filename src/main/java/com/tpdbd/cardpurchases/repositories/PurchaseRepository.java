fb78eb285b6acddaa2e61665a9c09bbbf4759640
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-06-14 20:56:54 -0300

    Finder findAllIds replaces the previous 2 finders

--- src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
@@ -2,11 +2,12 @@ package com.tpdbd.cardpurchases.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tpdbd.cardpurchases.model.Purchase;
 
 public interface PurchaseRepository<T extends Purchase> extends CrudRepository<T, Long> {
-    List<T> findByCardNumber(String cardNumber);
-    List<T> findByCardNumberAndCuitStore(String cardNumber, String cuitStore);
+    @Query("SELECT u.id FROM Purchase u")
+    List<Long> findAllIds();
 }
