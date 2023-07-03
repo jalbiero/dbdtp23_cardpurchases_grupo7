d4a760286a3ae0db724fec0ea43c804007b0b156
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-07-03 19:49:11 -0300

    Add findAllCodes and deleteByCode methods

--- src/main/java/com/tpdbd/cardpurchases/repositories/PromotionRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/PromotionRepository.java
@@ -3,10 +3,26 @@ package com.tpdbd.cardpurchases.repositories;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.tpdbd.cardpurchases.model.Promotion;
 
+import jakarta.transaction.Transactional;
+
 public interface PromotionRepository extends Repository<Promotion, Long> {
+   @Query("SELECT p.code FROM Promotion p")
+   List<String> findAllCodes();
+
    List<Promotion> findByCuitStoreAndValidityStartDateGreaterThanEqualAndValidityEndDateLessThanEqual(String cuitStore, LocalDate from, LocalDate to);
+
+   @Transactional
+   @Modifying
+   @Query(
+      "UPDATE Promotion p " + 
+      "SET p.deleted = true " + 
+      "WHERE p.code = :code")
+   Integer deleteByCode(@Param("code") String code);
 }
