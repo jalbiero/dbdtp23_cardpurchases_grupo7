563abfce5782c4827745e6d59513f31802f1a348
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-07-05 14:10:22 -0300

    Add findMonthlyPayment method

--- src/main/java/com/tpdbd/cardpurchases/repositories/PaymentRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/PaymentRepository.java
@@ -5,12 +5,22 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.tpdbd.cardpurchases.model.Payment;
 
 public interface PaymentRepository extends CrudRepository<Payment, Long> {
     Optional<Payment> findByCode(String code);
 
-    @Query("SELECT u.code FROM Payment u")
+    @Query("SELECT p.code FROM Payment p")
     List<String> findAllCodes();
+
+    @Query(
+        "SELECT p " + 
+        "FROM Payment p " + 
+        "WHERE p.card.number = :number AND p.year = :year AND p.month = :month")
+    Optional<Payment> findMonthlyPayment(@Param("number") String cardNumber,
+                                         @Param("year") int year, 
+                                         @Param("month") int month);
+
 }
