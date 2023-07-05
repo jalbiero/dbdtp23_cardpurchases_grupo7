e6f68a6b17f3f204c3f1a3ddd51bd86629df08ff
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-07-05 14:10:00 -0300

    Complete documentation for cardsGetMonthtlyPayment controller

--- src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
+++ src/main/java/com/tpdbd/cardpurchases/controllers/CardPurchasesController.java
@@ -119,13 +119,42 @@ public class CardPurchasesController {
      *          }
      * 
      * Return:
-     *      TODO Complete return type
+     *      {
+     *          "year": 2021,
+     *          "month": 8,
+     *          "totalPrice": 72423.0,
+     *          "purchase": [
+     *             {
+     *                 "id": 2,
+     *                 "cardNumber": "5876-1948-6884-1575",
+     *                 "PaymentVoucher": null,
+     *                 "store": "Florentin S.A.",
+     *                 "cuitStore": "14",
+     *                 "ammount": 36740.8,
+     *                 "finalAmount": 36740.8,
+     *                 "quotas": [
+     *                     {
+     *                         "number": 1,
+     *                         "price": 36740.8,
+     *                         "month": 8,
+     *                         "year": 2021,
+     *                         "store": "Florentin S.A.",
+     *                         "cardNumber": "5876-1948-6884-1575"
+     *                     }
+     *                 ],
+     *                 "storeDiscount": 0.0,
+     *                 "type": "CashPurchase"
+     *              },
+     *              ...
+     *          ]
+     *      }
      */
     @GetMapping("/cards/{number}/montlyPayment")
     ResponseDTO.MonthlyPayment cardsGetMonthtlyPayment(@PathVariable String number, 
                                                        @RequestBody RequestDTO.CardsMonthtlyPayment body) 
     {
-        return this.service.cardsGetMonthtlyPayment(number, body.year(), body.month());
+        return ResponseDTO.MonthlyPayment.fromModel(
+            this.service.cardsGetMonthtlyPayment(number, body.year(), body.month()));
     }
 
     /**
@@ -236,7 +265,6 @@ public class CardPurchasesController {
     void promotionsDelete(@PathVariable String code) {
         this.service.promotionsDelete(code);
     }
-
 
     /**
      * 08 - List promotions for the specified store in the specified date range
