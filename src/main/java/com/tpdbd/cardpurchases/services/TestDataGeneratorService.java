1de743824852cb10d8129abc861b68439435c1cd
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-07-09 23:03:35 -0300

    Remove unused import

--- src/main/java/com/tpdbd/cardpurchases/services/TestDataGeneratorService.java
+++ src/main/java/com/tpdbd/cardpurchases/services/TestDataGeneratorService.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.util.Random;
 import java.util.TreeSet;
 import java.util.function.Function;
