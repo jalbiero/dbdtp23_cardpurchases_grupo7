6eb4a67e8dbc6ee1ebb793cfbdc1266bca11ab92
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-07-18 16:56:30 -0300

    Move Purchase services to its own files

---
+++ src/main/java/com/tpdbd/cardpurchases/services/PurchaseService.java
@@ -0,0 +1,16 @@
+package com.tpdbd.cardpurchases.services;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import com.tpdbd.cardpurchases.model.Purchase;
+import com.tpdbd.cardpurchases.repositories.projections.MostUsedVoucher;
+import com.tpdbd.cardpurchases.repositories.projections.NumOfPurchasesByCard;
+
+public interface PurchaseService {
+    Optional<Purchase> findById(long purchaseId);
+
+    Stream<NumOfPurchasesByCard> findTopPurchasers(int count);
+
+    Stream<MostUsedVoucher> findTheMostUsedVouchers(int count);
+}
---
+++ src/main/java/com/tpdbd/cardpurchases/services/impl/PurchaseServiceImpl.java
@@ -0,0 +1,38 @@
+package com.tpdbd.cardpurchases.services.impl;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.tpdbd.cardpurchases.errors.NotFoundException;
+import com.tpdbd.cardpurchases.model.Purchase;
+import com.tpdbd.cardpurchases.repositories.PurchaseRepository;
+import com.tpdbd.cardpurchases.repositories.projections.MostUsedVoucher;
+import com.tpdbd.cardpurchases.repositories.projections.NumOfPurchasesByCard;
+import com.tpdbd.cardpurchases.services.PurchaseService;
+
+@Service
+public class PurchaseServiceImpl implements PurchaseService {
+    @Autowired
+    private PurchaseRepository<Purchase> purchaseRepository; 
+
+    @Override
+    public Optional<Purchase> findById(long purchaseId) {
+        return this.purchaseRepository.findById(purchaseId);
+    }
+
+    @Override
+    public Stream<NumOfPurchasesByCard> findTopPurchasers(int count) {
+        var topPurchaserCards = this.purchaseRepository.findTopPurchaserCards(PageRequest.of(0, count));
+        return topPurchaserCards.get();
+    }
+
+    @Override
+    public Stream<MostUsedVoucher> findTheMostUsedVouchers(int count) {
+        var mostUsedVouchers = this.purchaseRepository.findTheMostUsedVouchers(PageRequest.of(0, count));
+        return mostUsedVouchers.get();
+    }
+}
