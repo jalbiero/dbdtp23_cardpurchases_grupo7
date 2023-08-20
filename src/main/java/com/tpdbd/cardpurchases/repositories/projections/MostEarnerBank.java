ded523c0df4e99fb982c0ead9d563660df7c439b
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-08-19 21:59:59 -0300

    Make projections compatible con Mongo

--- src/main/java/com/tpdbd/cardpurchases/repositories/projections/MostEarnerBank.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/projections/MostEarnerBank.java
@@ -2,7 +2,17 @@ package com.tpdbd.cardpurchases.repositories.projections;
 
 import com.tpdbd.cardpurchases.model.Bank;
 
-public interface MostEarnerBank {
-    float getTotalPaymentValue();
-    Bank getBank();
+// Mongo projections don't work with interfaces, this record replaces the interface
+public record MostEarnerBank(Bank bank, float totalPaymentValue)  {
+
+    // Provides compatibility with the SQL version based on the 
+    // old NumOfPurchasesByCard interface
+
+    public Bank getBank() {
+        return this.bank;
+    }
+
+    public float getTotalPaymentValue() {
+        return this.totalPaymentValue;
+    }
 }
