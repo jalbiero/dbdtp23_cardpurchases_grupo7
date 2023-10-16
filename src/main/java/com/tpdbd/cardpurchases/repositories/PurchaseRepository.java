96b7e29b48f7927ae8107c295f9d42e3cb1b8a1d
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-10-16 00:36:58 -0300

    Add TODO for findTheMostUsedVouchers

--- src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
+++ src/main/java/com/tpdbd/cardpurchases/repositories/PurchaseRepository.java
@@ -11,21 +11,23 @@ import com.tpdbd.cardpurchases.repositories.projections.NumOfPurchasesByCard;
 
 public interface PurchaseRepository<T extends Purchase> extends CrudRepository<T, Long> {
     @Query(
-        "SELECT " + 
-        "   COUNT(p) AS numOfPurchases, " + 
-        "   p.card AS card " + 
-        "FROM Purchase p " + 
+        "SELECT " +
+        "   COUNT(p) AS numOfPurchases, " +
+        "   p.card AS card " +
+        "FROM Purchase p " +
         "GROUP BY p.card " +
         "ORDER BY numOfPurchases DESC")
-    Page<NumOfPurchasesByCard> findTopPurchaserCards(Pageable pageable);  
-    
+    Page<NumOfPurchasesByCard> findTopPurchaserCards(Pageable pageable);
+
+    // TODO This method will be moved to PromotionRepository when the missing
+    //      relationship Promotion <-1--n-> Purchase will be added
     @Query(
-        "SELECT " + 
-        "   COUNT(p) AS numOfPurchases, " + 
-        "   p.paymentVoucher AS code " + 
-        "FROM Purchase p " + 
+        "SELECT " +
+        "   COUNT(p) AS numOfPurchases, " +
+        "   p.paymentVoucher AS code " +
+        "FROM Purchase p " +
         "WHERE p.paymentVoucher IS NOT NULL " +
         "GROUP BY p.paymentVoucher " +
         "ORDER BY numOfPurchases DESC")
-    Page<MostUsedVoucher> findTheMostUsedVouchers(Pageable pageable);  
+    Page<MostUsedVoucher> findTheMostUsedVouchers(Pageable pageable);
 }
