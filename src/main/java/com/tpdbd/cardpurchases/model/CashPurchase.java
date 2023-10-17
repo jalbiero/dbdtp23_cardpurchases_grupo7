9f03f12646eacb7e02e2da87526c9f1aa836cbdd
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2023-10-16 23:17:31 -0300

    Add missing relationship with Payment

--- src/main/java/com/tpdbd/cardpurchases/model/CashPurchase.java
+++ src/main/java/com/tpdbd/cardpurchases/model/CashPurchase.java
@@ -1,5 +1,7 @@
 package com.tpdbd.cardpurchases.model;
 
+import java.util.Optional;
+
 import jakarta.annotation.Nullable;
 
 import org.springframework.data.annotation.TypeAlias;
@@ -11,29 +13,35 @@ public class CashPurchase extends Purchase {
 
     private float storeDiscount;
 
+    // Note: Payment is nullable because the chicken and egg problem:
+    //       1st: the purchase is made, 2nd: the payment is made
+    @Nullable
+    private Payment payment;
+
+    private int month;
+
+    private int year;
+
     public CashPurchase() {
     }
 
     public CashPurchase(
-        Card card, 
-        @Nullable String paymentVoucher, 
-        String store, 
-        String cuitStore, 
-        float amount, 
+        Card card,
+        @Nullable String paymentVoucher,
+        String store,
+        String cuitStore,
+        float amount,
         float finalAmount,
-        float storeDiscount) 
+        float storeDiscount,
+        @Nullable Payment payment,
+        int month,
+        int year)
     {
         super(card, paymentVoucher, store, cuitStore, amount, finalAmount);
         this.storeDiscount = storeDiscount;
-    }
-
-    @Override
-    public boolean addQuota(Quota quota) {
-        // Cash purchases only have 1 quota
-        if (getQuotas().size() == 0)
-            return super.addQuota(quota);
-
-        return false;
+        this.payment = payment;
+        this.month = month;
+        this.year = year;
     }
 
     public float getStoreDiscount() {
@@ -44,4 +52,27 @@ public class CashPurchase extends Purchase {
         this.storeDiscount = storeDiscount;
     }
 
+    public Optional<Payment> getPayment() {
+        return Optional.of(this.payment);
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public int getMonth() {
+        return this.month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
